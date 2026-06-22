819c4326b8540e4f335caafe633520647a417ce5
João Guedes <jguedes2408@gmail.com>
2026-06-22 00:36:53 -0300

    final da 05

--- js/005.js
+++ js/005.js
@@ -1,17 +1,10 @@
 
 
-const inputNome = document.querySelector('#nome')
-
-
 const inputRenda = document.querySelector('#renda')
-
 const inputIdade = document.querySelector('#idade')
-
 const inputSexo = document.querySelector('#sexo')
 
-
 const btnNum1 = document.querySelector('#btn-num1')
-
 const divResult = document.querySelector('#result')
 
 var contPessoasF = 0
@@ -22,45 +15,63 @@ var contRendaMulheres = 0
 var contRendaMasculino = 0
 var media = 0
 
-btnNum1.addEventListener('click', (evt) => {
-   
-    var pessoas = Number(inputNome.value)
-    var rendam = Number(inputRenda.value)
-    var rendaf = Number(inputRenda.value)
-    
-    
-    
-    if((rendaf >= 1000 && 3000) ){
+btnNum1.addEventListener('click', () => {
+    const idade = Number(inputIdade.value)
+    const renda = Number(inputRenda.value)
+    const sexo = inputSexo.value.trim().toUpperCase()
 
-        contRendaMulheres++    
-}
+    if (isNaN(idade) || isNaN(renda) || sexo === '') {
+        alert('Preencha idade, sexo e renda corretamente.')
+        return
+    }
 
-    if(rendam )
-   
- if((sexo == "masculino") || (sexo == "MASCULINO")) {
-    contPessoasM++
- }
- else if((sexo == "feminino") || (sexo == "MASCULINO")){
-    contPessoasF++
-}
+    totalPessoas++
 
+    if (sexo === 'M' || sexo === 'MASCULINO') {
+        contPessoasMasculino++
+        somaSalariosMasculinos += renda
 
+        if (renda >= 2000) {
+            contMasculinoRendaAcima2000++
+        }
+    } else if (sexo === 'F' || sexo === 'FEMININO') {
+        contPessoasFeminino++
 
+        if (renda >= 1000 && renda <= 3000) {
+            contMulheresRendaEntre1000E3000++
+        }
 
-    divResult.innerHTML = `pessoas digitadas: ${contPessoasF + contPessoasM} <br> <br>
-    media salarial masculina: ${(media + '#renda') % contPessoasM  } <br> <br>
-    pessoas entre 18 e 65: ${contQuantidade} <br> <br>
-    pessoas masculinas: ${contPessoasM} <br> <br>
-    pessoas renda femininas a cima de 1000: ${contRendaMulheres}`
+        if (renda >= 2000) {
+            contFemininoRendaAcima2000++
+        }
+    } else {
+        alert('Sexo inválido. Use M ou F (ou Masculino/Feminino).')
+        totalPessoas--
+        return
+    }
 
+    const mediaSalarioMasculino = contPessoasMasculino > 0
+        ? (somaSalariosMasculinos / contPessoasMasculino).toFixed(2)
+        : '0.00'
 
-    
+    const percentualMasculino = contPessoasMasculino > 0
+        ? ((contMasculinoRendaAcima2000 / contPessoasMasculino) * 100).toFixed(2)
+        : '0.00'
+
+    const percentualFeminino = contPessoasFeminino > 0
+        ? ((contFemininoRendaAcima2000 / contPessoasFeminino) * 100).toFixed(2)
+        : '0.00'
+
+    divResult.innerHTML = `
+        Pessoas digitadas: ${totalPessoas} <br><br>
+        Média salarial masculina: R$ ${mediaSalarioMasculino} <br><br>
+        Mulheres com renda entre R$ 1000,00 e R$ 3000,00: ${contMulheresRendaEntre1000E3000} <br><br>
+        Percentual masculino com renda >= R$ 2000,00: ${percentualMasculino}% <br><br>
+        Percentual feminino com renda >= R$ 2000,00: ${percentualFeminino}%
+    `
+
     inputIdade.value = ''
     inputSexo.value = ''
-    inputNome.value = ''
-
-
-
-
+    inputRenda.value = ''
 })
     
